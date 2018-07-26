c5bf7c9341346f3e462fa4d56fd87d711c4ecf11
Jerry <shengfengzhao@icloud.com>
2018-07-26 19:53:14 +0800

    首页界面

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button } from '@tarojs/components'
+import { View, Button, Text, Input, Slider, ScrollView } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
@@ -42,14 +42,42 @@ class Index extends Component {
         <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
         <View>{this.props.counter.num}</View>
         <View>Hello, World</View> */}
-        <Text>目的地:</Text>
-        <Input type='text' placeholder='将要去的目的地' focus/>
-        <Text>交通工具:</Text>
-        <Input type='text' placeholder='所搭乘的交通工具' focus/>
-        <Text>定位频率(秒):</Text>
-        <Slider step="5" value="30" showValue min="5" max="120"/>
-        <Button className='' style="">我</Button>
-        <Button className='' style="">发起行程</Button>
+
+        {/* <View style='height:150px;background-color:rgb(26,173,25);'>A</View>
+          <View style='height:150px;background-color:rgb(39,130,215);'>B</View>
+          <View style='height:150px;background-color:rgb(241,241,241);color: #333;'>C</View> */}
+
+        <ScrollView className='scrollview'
+            scrollY
+            scrollWithAnimation
+            scrollTop='0'
+            style='flex: 1;'
+            lowerThreshold='20'
+            upperThreshold='20'>
+
+          <div style='display:flex; flex-direction: row; height: 44px; align-items: center;'>
+            <Text style='width: 33vw; margin-left: 8px;'>目的地:</Text>
+            <Input style='flex: 1; margin-right: 8px;' type='text' placeholder='将要去的目的地' />
+          </div>
+          
+          <div style='display:flex; flex-direction: row; height: 44px; align-items: center;'>
+            <Text style='width: 33vw; margin-left: 8px;'>交通工具:</Text>
+            <Input style='flex: 1; margin-right: 8px;' type='text' placeholder='所搭乘的交通工具' />
+          </div>
+          
+          <div style='display:flex; flex-direction: row; height: 44px; align-items: center;'>
+            <Text style='width: 33vw; margin-left: 8px;'>定位频率(秒):</Text>
+            <Slider style='flex: 1; margin-right: 8px;' step="5" value="30" showValue min="5" max="120"/>
+          </div>
+
+          <view style='height: 88px;'></view>
+        </ScrollView>
+
+        {/* <Button className='' style="">我</Button> */}
+        
+        <div style='position:fixed; height: 66px; width: 100vw; bottom: 0; z-index:99; background-color:yellow;'>
+          <Button className='' style='height: 66px; width: 100vw;' size='default' plain='true'>发起行程</Button>
+        </div>
       </View>
     )
   }
