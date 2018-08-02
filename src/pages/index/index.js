7752e16e56c4501a9a03082e3bdce12b76048614
Jerry <shengfengzhao@icloud.com>
2018-08-02 16:23:32 +0800

    清理无用代码

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -139,43 +139,6 @@ class Index extends Component {
           <View style='height:150px;background-color:rgb(39,130,215);'>B</View>
           <View style='height:150px;background-color:rgb(241,241,241);color: #333;'>C</View> */}
 
-        
-        {/*
-        <ScrollView className='scrollview'
-            scrollY
-            scrollWithAnimation
-            scrollTop='0'
-            style='flex: 1;'
-            lowerThreshold='20'
-            upperThreshold='20'>
-
-          <div style='display:flex; flex-direction: row; height: 44px; align-items: center;'>
-            <Text style='width: 33vw; margin-left: 8px;'>目的地:</Text>
-            <Input style='flex: 1; margin-right: 8px; border-style: solid; border-width: 0.5px; border-radius: 8px; border-color: rgba(9, 153, 255, 0.25);' type='text' placeholder='将要去的目的地' />
-          </div>
-          
-          <div style='display:flex; flex-direction: row; height: 44px; align-items: center;'>
-            <Text style='width: 33vw; margin-left: 8px;'>交通工具:</Text>
-            <Input style='flex: 1; margin-right: 8px;' type='text' placeholder='所搭乘的交通工具' />
-          </div>
-          
-          <div style='display:flex; flex-direction: row; height: 44px; align-items: center;'>
-            <Text style='width: 33vw; margin-left: 8px;'>定位频率(秒):</Text>
-            <Slider style='flex: 1; margin-right: 8px;' step="5" value="30" showValue min="5" max="120"/>
-          </div>
-
-          <view style='height: 88px;'></view>
-        </ScrollView>
-
-        <Button className='' style="">我</Button>
-        
-        <div style='position:fixed; height: 66px; width: 100vw; bottom: 0; z-index:99; background-color:yellow;'>
-          <Button className='' style='height: 66px; width: 100vw;' size='default' plain='true'>发起行程</Button>
-        </div>
-        */}
-
-        
-
       </View>
     )
   }
